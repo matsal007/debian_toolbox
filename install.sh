f6997b884596325699d3a60824ad8c79944c3022
matsal007 <salvadego2007@gmail.com>
2024-05-17 17:34:46 -0300

    [main] - 'Fixed bin path'

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ DOWNLOAD_COLOR='\033[1;35m'
 SUCCESS_COLOR='\033[1;32m'
 ERROR_COLOR='\033[1;31m'
 
-HOME="~/"
+BIN_PATH=$HOME/.local/bin
 
 echo_info(){
     echo -e "${INFO_COLOR}[INFO] | $1${NO_COLOR}"
@@ -33,15 +33,15 @@ indeb(){
     ar -x *.deb || echo_error "$package_name unpacking failed"
     tar -xvf data.tar.xz || echo_error "$package_name unpacking data.tar.xa failed"
 
-    echo_info "Moving $package_name to $HOME/.local/bin"
-    mv usr/bin/* "$HOME"/.local/bin && echo_succes "$package_name installed"
+    echo_info "Moving $package_name to ~/.local/bin"
+    mv usr/bin/* ~/.local/bin && echo_succes "$package_name installed"
     rm -rf * 
 }
 
 downloadBig5(){
     echo_download "fzf"
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.local/bin/fzf || echo_error "fzf download failed"
-    "$HOME"/.local/bin/fzf/install || echo_error "fzf install failed"
+    "$BIN_PATH"/fzf/install || echo_error "fzf install failed"
 
     echo_download "tree"
     wget http://ftp.debian.org/debian/pool/main/t/tree/tree_1.8.0-1+b1_amd64.deb -P /tmp/deb/ || echo_error "tree download failed"
@@ -51,15 +51,16 @@ downloadBig5(){
     wget https://github.com/sharkdp/bat/releases/download/v0.24.0/bat_0.24.0_amd64.deb -P /tmp/deb/ || echo_error "bat download failed"
     echo_download "ripgrep"
     wget https://github.com/BurntSushi/ripgrep/releases/download/14.1.0/ripgrep_14.1.0-1_amd64.deb -P /tmp/deb/ || echo_error "ripgrep download failed"
+
 }
 
 downloadNeovim(){
     echo_download "neovim-nightly"
-    wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -O "$HOME/.local/bin/nvim" || echo_error "neovim-nightly download failed"
-    chmod +x "$HOME"/.local/bin/nvim && echo_succes "neovim-nightly installed"
+    wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -O "$BIN_PATH"/nvim || echo_error "neovim-nightly download failed"
+    chmod +x ~/.local/bin/nvim && echo_succes "neovim-nightly installed"
 }
 
-mkdir -p "$HOME"/.local/bin
+mkdir -p $BIN_PATH
 
 downloadNeovim
 mkdir -p /tmp/deb/installation
@@ -74,6 +75,6 @@ do
     indeb $i
 done
 
-[[ ! "$PATH" =~ "$HOME/.local/bin" ]] && echo "export PATH='$PATH:$HOME/.local/bin'" >> ~/.bashrc
+[[ ! "$PATH" =~ $BIN_PATH ]] && echo "export PATH='$PATH:$BIN_PATH'" >> ~/.bashrc
 
 $SHELL
